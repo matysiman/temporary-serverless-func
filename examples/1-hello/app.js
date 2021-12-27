b838430d6682b2bc4532bc7c405f5c463a60045d
Maty Siman <maty.siman@checkmarx.com>
2021-12-27 17:56:15 +0200

    1-hello update

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -5,6 +5,7 @@ const fetchData = async () => {
     try {
      const {data} = await axios.get ('/api/1-hello')
      result.textContent = data;
+     result.innerHTML = '<h2>successדדדדדדדדדד</h2>'
     } catch (error) {
         result.textContent = "Error: " + error.response.data;
     }
