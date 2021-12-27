499f8392375012a4abf70cf5b0e8d1a2f2a51670
Maty Siman <maty.siman@checkmarx.com>
2021-12-27 18:34:52 +0200

    covidtest

--- examples/1-hello/app.js
+++ examples/1-hello/app.js
@@ -5,7 +5,7 @@ const fetchData = async () => {
     try {
      const {data} = await axios.get ('/api/1-hello')
      result.textContent = data;
-     result.innerHTML = '<h2>successדדדדדדדדדד</h2>'
+     result.innerHTML = '<h2>COVID!!!</h2>'
     } catch (error) {
         result.textContent = "Error: " + error.response.data;
     }
