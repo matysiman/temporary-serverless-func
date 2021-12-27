f07ca0b7224e8379be7f506291edf461a97ee952
Maty Siman <maty.siman@checkmarx.com>
2021-12-27 17:44:19 +0200

    6th commit

--- examples/2-basic-api/app.js
+++ examples/2-basic-api/app.js
@@ -18,7 +18,7 @@ const fetchData = async () => {
         />
         <div class="info">
           <h5>${name}</h5>
-          <h5 class="price">$${price} prs</h5>
+          <h5 class="price">$${price} prsssss</h5>
         </div>
       </article>`
     
